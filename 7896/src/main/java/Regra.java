d1ef4b95110aa627f613dfd991cd7d31d906fb67
Eduardo_Macedo <errmm@iscte-iul.pt>
2020-11-15 17:03:46 +0000

    classe regra_inicio

--- 7896/src/main/java/Regra.java
+++ 7896/src/main/java/Regra.java
@@ -1,10 +1,20 @@
 
 public class Regra {
 	
-	
+	private Metrica metrica;
+	private int threshold;
 
-	public Regra() {
-		// TODO Auto-generated constructor stub
+	public Regra(Metrica metrica, int threshold) {
+		this.metrica = metrica;
+		this.threshold = threshold;
+	}
+	
+	public void AND(Metrica m1, Metrica m2, Metrica m3) {
+		
+	}
+	
+	public void OR(Metrica m1, Metrica m2, Metrica m3) {
+		
 	}
 
 }
